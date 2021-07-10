9ab0c4849c4dddf58d07d58de2fbb5175d32b287
Kevin Mau <85687829+kevin-mau@users.noreply.github.com>
2021-07-10 09:26:30 -0700

    added tests for the filters

--- tests/test_qualifier.py
+++ tests/test_qualifier.py
@@ -14,26 +14,32 @@ from qualifier.filters import debt_to_income
 from qualifier.filters import loan_to_value
 from qualifier.filters import max_loan_size
 def test_save_csv():
-    path = Path('./data/output/qualifying_loans.csv')
+    path = Path('data\output\qualifying_loans.csv')
     assert (path).exists()
-
 def test_calculate_monthly_debt_ratio():
     assert calculators.calculate_monthly_debt_ratio(1500, 4000) == 0.375
-
 def test_calculate_loan_to_value_ratio():
     assert calculators.calculate_loan_to_value_ratio(210000, 250000) == 0.84
 
 def test_filters():
-    bank_data = fileio.load_csv(Path('./data/daily_rate_sheet.csv'))
-    current_credit_score = 750
+    bank_data = fileio.load_csv(Path('data\daily_rate_sheet.csv'))
+    current_credit_score = 100
     debt = 1500
     income = 4000
     loan = 210000
     home_value = 250000
-
+    '''assert len(credit_score(current_credit_score, bank_data)) == 0
     monthly_debt_ratio = 0.375
-
+    assert len(debt_to_income(monthly_debt_ratio, bank_data)) == 5
     loan_to_value_ratio = 0.84
-
+    assert len(loan_to_value(loan_to_value_ratio, bank_data)) == 5
+    assert len(max_loan_size(loan, bank_data)) == 5
+'''
     # @TODO: Test the new save_csv code!
     # YOUR CODE HERE!
+
+    #output =  Path('data\output\qualifying_loans.csv')
+    
+    #assert len(fileio.save_csv(bank_data, output, True)) == 24
+
+
